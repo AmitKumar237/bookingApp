3dbbec3247aaef9041e0c0325410141a2bfdd92a
Amit Kumar <723kumaramit@gmail.com>
2023-05-30 11:40:52 +0530

    Login Register UI

---
+++ client/src/components/Login.jsx
@@ -0,0 +1,18 @@
+import {Link} from 'react-router-dom'
+export default function Login() {
+    return(
+        <div className="mt-4 grow flex items-center justify-around">
+            <div className="mb-32">
+                <h1 className="text-4xl text-center mb-4">Login</h1>
+                <form action="" className="max-w-md mx-auto">
+                    <input type="email" placeholder="your@email.com"/>
+                    <input type="password" placeholder="password" />
+                    <button className="primary">Login</button>
+                    <div className='text-gray-500 text-center py-2'>
+                        Don't have the account yet? <Link to={'/register'} className='underline text-black'>Register now</Link>
+                    </div>
+                </form>
+            </div>
+        </div>
+    )
+}
---
+++ client/src/components/Register.jsx
@@ -0,0 +1,19 @@
+import {Link} from 'react-router-dom'
+export default function Register() {
+    return(
+        <div className="mt-4 grow flex items-center justify-around">
+            <div className="mb-32">
+                <h1 className="text-4xl text-center mb-4">Register</h1>
+                <form action="" className="max-w-md mx-auto">
+                    <input type="text" placeholder='John Doe'/>
+                    <input type="email" placeholder="your@email.com"/>
+                    <input type="password" placeholder="password" />
+                    <button className="primary">Register</button>
+                    <div className='text-gray-500 text-center py-2'>
+                        Already have an account? <Link to={'/register'} className='underline text-black'>Login now</Link>
+                    </div>
+                </form>
+            </div>
+        </div>
+    )
+}
